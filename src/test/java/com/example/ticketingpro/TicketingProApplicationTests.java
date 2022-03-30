bf44a33b275e1db41e477a3a19c5da71e0dc1040
adam0nur <adamnur0117@gmail.com>
2022-03-29 22:55:55 -0500

    Initial commit

---
+++ src/test/java/com/example/ticketingpro/TicketingProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketingpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
